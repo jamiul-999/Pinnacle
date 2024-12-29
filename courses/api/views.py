06fbd2ac7f58dbe7cf56a136b0fa5449e8626465
charlie <jhdhj999@gmail.com>
2024-12-29 19:38:01 +0600

    Serializers extended

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,11 +1,14 @@
 from rest_framework import generics
 from courses.api.serializers import SubjectSerializer
+from django.db.models import Count
+
 from courses.models import Subject
 
+
 class SubjectListView(generics.ListAPIView):
-    queryset = Subject.objects.all()
+    queryset = Subject.objects.annotate(total_courses=Count("courses"))
     serializer_class = SubjectSerializer
 
 class SubjectDetailView(generics.RetrieveAPIView):
-    queryset = Subject.objects.all()
+    queryset = Subject.objects.annotate(total_courses=Count("courses"))
     serializer_class = SubjectSerializer
