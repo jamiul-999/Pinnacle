d6125a78ef922f14c4f6a34ceeab98dfbf8c6220
charlie <jhdhj999@gmail.com>
2024-12-29 19:47:36 +0600

    Pagination added

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,13 +1,14 @@
-from rest_framework import generics
-from courses.api.serializers import SubjectSerializer
 from django.db.models import Count
-
+from rest_framework import generics
 from courses.models import Subject
+from courses.api.pagination import StandardPagination
+from courses.api.serializers import SubjectSerializer
 
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.annotate(total_courses=Count("courses"))
     serializer_class = SubjectSerializer
+    pagination_class = StandardPagination
 
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.annotate(total_courses=Count("courses"))
