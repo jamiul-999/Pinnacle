448e88ce4c5b59f7d3914a0868792c4fc0effb07
charlie <jhdhj999@gmail.com>
2024-12-29 20:57:50 +0600

    Created custom API views

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,9 @@
 from django.db.models import Count
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from courses.models import Course, Subject
 from courses.api.pagination import StandardPagination
 from courses.api.serializers import CourseSerializer, SubjectSerializer
@@ -23,3 +26,9 @@ class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Subject.objects.annotate(total_courses=Count("courses"))
     serializer_class = SubjectSerializer
     pagination_class = StandardPagination
+    
+class CourseEnrollView(APIView):
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({"enrolled": True})
