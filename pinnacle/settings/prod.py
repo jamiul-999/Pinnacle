7e992f17be7ac816d845ef421ef0639b493dfc08
charlie <jhdhj999@gmail.com>
2025-01-02 20:52:57 +0600

    Production environment created with docker compose and postgresql

---
+++ pinnacle/settings/prod.py
@@ -0,0 +1,25 @@
+from decouple import config
+from .base import *
+
+DEBUG = False
+
+ADMINS = [
+    ("JHD", "jhdhj999@gmail.com"),
+]
+
+ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": 5432,
+    }
+}
+
+REDIS_URL = "redis://cache:6379"
+CACHES["default"]["LOCATION"] = REDIS_URL
+CHANNEL_LAYERS["default"]["CONFIG"]["hosts"] = [REDIS_URL]
