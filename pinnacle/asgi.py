3cc73b0a81b739f4c6bfcf1dfd999a8ed5c0e2ea
charlie <jhdhj999@gmail.com>
2025-01-06 13:45:25 +0600

    Production environment created through uwsgi and nginx & custom middleware created

--- pinnacle/asgi.py
+++ pinnacle/asgi.py
@@ -8,19 +8,21 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pinnacle.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pinnacle.settings.prod")
 
 django_asgi_app = get_asgi_application()
-
 from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        )
     ),
 })
